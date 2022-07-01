ad180f05a5e9f9a7fec56154a8f0beee80b13605
TaeyeonKim92 <108449975+TaeyeonKim92@users.noreply.github.com>
2022-07-01 11:21:14 +0900

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,57 @@
+
+// 변수 하나 입력 받기 
+
+// import java.util.*;    //scanenr 사용하기 위해 추가
+
+// class lowerlight {
+//   public static void main(String[] args) {
+//     Scanner stdIn = new Scanner(System.in); //입력 받기
+
+//     System.out.print("정수값: "); 
+//     int x = stdIn.nextInt();  
+//     int y = x % 10; //10으로 나눈 나머지 연산자 : % 2n+1, 2n 활용해서 홀/짝수 구분  
+//     int z = x / 10; //10으로 나눈 몫
+
+
+//     System.out.println("마지막 자릿수를 제외한 값은 " + z + "입니다");
+//     System.out.println("마지막 자릿수는 " + y + "입니다.");
+//   }
+// }
+
+//변수 두개 입력받기
+
+// import java.util.*;
+
+// class SumAveDouble {
+//   public static void main(String[] args) {
+//     Scanner value1  = new Scanner(System.in);
+
+//     System.out.print("x값: ");
+//     double x = value1.nextDouble();
+//     System.out.print("y값: ");  
+//     double y = value1.nextDouble();
+
+//     System.out.println("합계는 " + (x+y)+ "입니다");
+//     System.out.println("평균은 " + (x+y)/2 + "입니다");
+
+// //   }
+// // }
+
+// //삼각형 넓이 구하기
+
+// import java.util.*;
+
+// class Triangle{
+//   public static void main(String[] args) {
+//     Scanner value1 = new Scanner(System.in);
+    
+
+//     System.out.println("삼각형의 넓이를 구합니다");
+//     System.out.print("밑변: ");
+//     double x = value1.nextDouble();
+//     System.out.print("높이: ");
+//     double y = value1.nextDouble();
+
+//     System.out.println("넓이는 "+ (x*y/2)+ "입니다.");
+//   }
+// }
